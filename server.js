c60d82ed7365e9d73ff2cdfac9323b0e1a70cf60
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2022-11-09 10:22:11 -0600

    setup db connection and view

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'baby-tracker'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db=client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(exppress.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-const PORT = 8000
+app.post('/addWeight', (req, res) => {
+
+})
+
+
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on port ${PORT}. You better go catch it!`)
 })
